396291c58be230b3024f4d8995044359ed9ae0f3
eldeivisuwu <162366750+eldeivisuwu@users.noreply.github.com>
2024-03-05 13:13:31 -0600

    Initial commit

---
+++ 1. MVC/MVC/MVC/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Data.Context;
+
+namespace MVC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration) 
+        {
+           
+            services.AddDbContext<PvContext>(opt => 
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("PvConnection"));
+            }
+                );
+
+
+            return services;
+        }
+    }
+}
